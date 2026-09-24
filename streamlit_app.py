d3af5e2a686a21d86e655e708f66847ad8f82e10
lidiasanchez-06 <lidia.sanchez@bluetab.net>
2026-09-24 11:17:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests  
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -11,8 +13,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -20,11 +22,16 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
+
 if ingredients_list:
     ingredients_string = ""
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        st.subheader(fruit_chosen + " Nutrition Information")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     my_insert_stmt = f"""
         INSERT INTO SMOOTHIES.PUBLIC.ORDERS
